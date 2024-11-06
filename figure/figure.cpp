80869946ee6f6a9580c5bcb3ec1ab02522d82287
Aceyoi <146310875+Aceyoi@users.noreply.github.com>
2024-11-06 23:51:46 +0900

    Добавьте файлы проекта.

---
+++ figure/figure.cpp
@@ -0,0 +1,37 @@
+﻿// Код создан Сапожниковым Ю.С.
+// Используемые библиотеки
+
+#include <iostream>
+#include <cassert>
+#include <fstream> // Добавлено для работы с файлами
+#include "figureModul.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    RecTest(); //Тесты
+
+    float w, h, l;
+    // Укажите длину, ширину и высоту
+    cout << "Укажите длину: ";
+    cin >> l; // длина
+    cout << "Укажите ширину: ";
+    cin >> w; // ширина
+    cout << "Укажите высоту: ";
+    cin >> h; // высота
+
+    // Создаем экземпляр класса
+    Rectangle* figure = new Rectangle();
+
+    figure->setFigL(l);// Добавляем длину
+    figure->setFigW(w);// Добавляем ширину
+    figure->setFigH(h);// Добавляем высоту
+
+    printclass(figure); // Передаем объект в функцию вывода
+    VText(figure); // Передаем объект в функцию вывода в файл
+
+    delete figure; // Очистка памяти
+    return 0; // Завершение программы
+}
